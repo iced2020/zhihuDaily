e1a4522ecc999eee322a67ac524c94de3ca08ab7
iced2020 <1056462121@qq.com>
2022-02-27 13:02:05 +0800

    zhihuDaily

--- zhihuDaily/ViewController/ToolBarViewController.m
+++ zhihuDaily/ViewController/ToolBarViewController.m
@@ -33,9 +33,9 @@
     self.mToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0,0, SCREEN_WIDTH, toolbarHeight)];
     //初始化itemFirst,itemSecond,itemThird并修改一下图片的渲染模式（不然图片是蓝色的）
     //ps：图标图片应该采用矢量图吧 目前是直接挖的图片与bar还有色差
-    self.itemFirst = [[UIBarButtonItem alloc] initWithImage:[self reSizeImage:[UIImage imageNamed:@"back.jpg"] toSize:CGSizeMake(40, 40)] style:UIBarButtonItemStyleDone target:self action:@selector(exit)];
-    self.itemSecond = [[UIBarButtonItem alloc] initWithImage:[self reSizeImage:[UIImage imageNamed:@"like.jpg"] toSize:CGSizeMake(40, 40)] style:UIBarButtonItemStyleDone target:self action:@selector(like)];
-    self.itemThird = [[UIBarButtonItem alloc] initWithImage:[self reSizeImage:[UIImage imageNamed:@"star.jpg"] toSize:CGSizeMake(40, 40)] style:UIBarButtonItemStyleDone target:self action:@selector(star)];
+    self.itemFirst = [[UIBarButtonItem alloc] initWithImage:[self reSizeImage:[UIImage imageNamed:@"back.jpg"] toSize:CGSizeMake(80, 50)] style:UIBarButtonItemStyleDone target:self action:@selector(exit)];
+    self.itemSecond = [[UIBarButtonItem alloc] initWithImage:[self reSizeImage:[UIImage imageNamed:@"like.jpg"] toSize:CGSizeMake(45, 45)] style:UIBarButtonItemStyleDone target:self action:@selector(like)];
+    self.itemThird = [[UIBarButtonItem alloc] initWithImage:[self reSizeImage:[UIImage imageNamed:@"star.jpg"] toSize:CGSizeMake(45, 45)] style:UIBarButtonItemStyleDone target:self action:@selector(star)];
     [self getdata];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSLog(@"%@", self.popularity);
@@ -46,7 +46,7 @@
         UIBarButtonItem *flexibaleSpaceItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
         //将itemFirst、itemSecond等UIBarButtonItem添加到UIToolbar，并使用FlexibleSpace优化布局
         //总结一下Flexible Space:它能够让item之间的控件弹性变化，保证item之间的间距都是一样的
-        self.mToolbar.items = @[flexibaleSpaceItem, self.itemFirst, flexibaleSpaceItem,self.itemSecond, flexibaleSpaceItem, self.itemFourth, flexibaleSpaceItem,self.itemThird, flexibaleSpaceItem];
+        self.mToolbar.items = @[self.itemFirst, flexibaleSpaceItem,self.itemSecond, flexibaleSpaceItem, self.itemFourth, flexibaleSpaceItem,self.itemThird, flexibaleSpaceItem];
         [self.view addSubview:self.mToolbar];
         });
 }
@@ -96,7 +96,7 @@
 
 #pragma mark - 喜欢/取消喜欢 收藏/取消收藏
 - (void)like{
-    self.itemSecond.image = [self reSizeImage:[UIImage imageNamed:@"liked.jpg"] toSize:CGSizeMake(40, 40)];
+    self.itemSecond.image = [self reSizeImage:[UIImage imageNamed:@"liked.jpg"] toSize:CGSizeMake(45, 45)];
     self.itemSecond.action = @selector(unLike);
     self.popularity = [NSNumber numberWithInt:[self.popularity intValue] + 1];
     self.popularityNum = [self.popularity stringValue];
@@ -105,7 +105,7 @@
 }
 
 - (void)unLike{
-    self.itemSecond.image = [self reSizeImage:[UIImage imageNamed:@"like.jpg"] toSize:CGSizeMake(40, 40)];
+    self.itemSecond.image = [self reSizeImage:[UIImage imageNamed:@"like.jpg"] toSize:CGSizeMake(45, 45)];
     self.itemSecond.action = @selector(like);
     self.popularity = [NSNumber numberWithInt:[self.popularity intValue] - 1];
     self.popularityNum = [self.popularity stringValue];
@@ -114,13 +114,13 @@
 }
 
 - (void)star{
-    self.itemThird.image = [self reSizeImage:[UIImage imageNamed:@"stared.jpg"] toSize:CGSizeMake(40, 40)];
+    self.itemThird.image = [self reSizeImage:[UIImage imageNamed:@"stared.jpg"] toSize:CGSizeMake(45, 45)];
     self.itemThird.action = @selector(unStar);
     [MBProgressHUDTool showMessage:@"收藏成功"];
 }
 
 - (void)unStar{
-    self.itemThird.image = [self reSizeImage:[UIImage imageNamed:@"star.jpg"] toSize:CGSizeMake(40, 40)];
+    self.itemThird.image = [self reSizeImage:[UIImage imageNamed:@"star.jpg"] toSize:CGSizeMake(45, 45)];
     self.itemThird.action = @selector(star);
     [MBProgressHUDTool showMessage:@"取消收藏"];
 }
